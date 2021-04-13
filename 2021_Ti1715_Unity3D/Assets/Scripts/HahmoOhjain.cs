5761014ee33f53a486c45c41c8adce45464e37fe
kodarit11 <taavi.makela@gmail.com>
2021-04-13 18:15:56 +0300

    projekti aloitettu

---
+++ 2021_Ti1715_Unity3D/Assets/Scripts/HahmoOhjain.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HahmoOhjain : MonoBehaviour
+{
+    public float juoksuNopeus = 3.5f;
+    public float hiirenNopeus = 3f;
+    public float hyppyNopeus = 100f;
+    public float painovoima = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
