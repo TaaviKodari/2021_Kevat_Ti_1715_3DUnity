854620ddaadd2badf4be3797106bbc9ccee6f2be
kodarit11 <taavi.makela@gmail.com>
2021-04-20 18:11:58 +0300

    kamera pyörii

--- 2021_Ti1715_Unity3D/Assets/Scripts/HahmoOhjain.cs
+++ 2021_Ti1715_Unity3D/Assets/Scripts/HahmoOhjain.cs
@@ -8,6 +8,10 @@ public class HahmoOhjain : MonoBehaviour
     public float hiirenNopeus = 3f;
     public float hyppyNopeus = 100f;
     public float painovoima = 10f;
+
+    private float vertikaalinenPyorinta = 0;
+    private float horisontaalinenPyorinta = 0;
+    private Vector3 liikesuunta = Vector3.zero;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +21,9 @@ public class HahmoOhjain : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horisontaalinenPyorinta += Input.GetAxis("Mouse X") * hiirenNopeus;
+        vertikaalinenPyorinta -= Input.GetAxis("Mouse Y") * hiirenNopeus;
+        Camera.main.transform.localRotation = Quaternion.Euler(vertikaalinenPyorinta,horisontaalinenPyorinta,0);
     }
 
 }
