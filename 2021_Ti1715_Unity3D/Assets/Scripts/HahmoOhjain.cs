ca5fe7cfff63feacd739c687d99f559051078cb6
kodarit11 <taavi.makela@gmail.com>
2021-05-04 18:13:41 +0300

    hyppy ja katseleminen kuntoon

--- 2021_Ti1715_Unity3D/Assets/Scripts/HahmoOhjain.cs
+++ 2021_Ti1715_Unity3D/Assets/Scripts/HahmoOhjain.cs
@@ -8,14 +8,24 @@ public class HahmoOhjain : MonoBehaviour
     public float hiirenNopeus = 3f;
     public float hyppyNopeus = 100f;
     public float painovoima = 10f;
-
+    public float maxKaannosAsteet = 60;
+    public float minKaannosAsteet = -70;
+    public CursorLockMode haluttuMoodi;
     private float vertikaalinenPyorinta = 0;
     private float horisontaalinenPyorinta = 0;
     private Vector3 liikesuunta = Vector3.zero;
+
+    [SerializeField]
+    private CharacterController controller;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = haluttuMoodi;
+        // piilota kursori kun lukittu
+        Cursor.visible = (CursorLockMode.Locked != haluttuMoodi);
+
+        controller = GetComponent<CharacterController>();
+
     }
 
     // Update is called once per frame
@@ -23,7 +33,28 @@ public class HahmoOhjain : MonoBehaviour
     {
         horisontaalinenPyorinta += Input.GetAxis("Mouse X") * hiirenNopeus;
         vertikaalinenPyorinta -= Input.GetAxis("Mouse Y") * hiirenNopeus;
+        
+        vertikaalinenPyorinta = Mathf.Clamp(vertikaalinenPyorinta, minKaannosAsteet, maxKaannosAsteet);
+
         Camera.main.transform.localRotation = Quaternion.Euler(vertikaalinenPyorinta,horisontaalinenPyorinta,0);
+
+        float nopeusEteen = Input.GetAxis("Vertical");
+        float nopeusSivulle = Input.GetAxis("Horizontal");
+        Vector3 nopeus = new Vector3(nopeusSivulle * juoksuNopeus,0,nopeusEteen * juoksuNopeus);
+
+        nopeus = transform.rotation * nopeus;
+
+        controller.SimpleMove(nopeus);
+
+        liikesuunta.y -= painovoima * Time.deltaTime;
+        controller.Move(liikesuunta * Time.deltaTime);
+
+        if(controller.isGrounded && Input.GetButtonDown("Jump"))
+        {
+            liikesuunta.y += hyppyNopeus;
+        }
+
     }
+
 
 }
